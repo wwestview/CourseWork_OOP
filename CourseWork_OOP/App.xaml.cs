f874c006b419861ef995022322be044fb5e54396
wwestview <biokapt@gmail.com>
2025-04-13 16:03:15 +0300

    Files

---
+++ CourseWork_OOP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CourseWork_OOP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
