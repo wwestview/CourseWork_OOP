29c97d99f23419bcd2d467e2dbcf4b3843ce28c0
wwestview <biokapt@gmail.com>
2025-05-16 15:31:11 +0300

    Add BaseTitlePageGenerator

---
+++ CourseWork_OOP/Services/BaseTitlePageGenerator.cs
@@ -0,0 +1,19 @@
+﻿using CourseWork_OOP.Interfaces;
+using CourseWork_OOP.Services; 
+using System.Threading.Tasks;
+
+namespace CourseWork_OOP.Services
+{
+    public abstract class BaseTitlePageGenerator : ITitlePageGenerator
+    {
+        public abstract string FormatName { get; }
+        public abstract string FileExtension { get; }
+
+        protected static string GetText(string? text, string defaultText = "")
+        {
+            return string.IsNullOrWhiteSpace(text) ? defaultText : text;
+        }
+
+        public abstract Task GenerateAsync(TitlePageData data, string argument);
+    }
+}
